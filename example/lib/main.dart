4ab09568d2c0a6ff7daf21066ff8027728e2646b
mehmetefeozcan <ozcanmehmetefe@gmail.com>
2023-06-20 10:36:04 +0300

    done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_customizable_image_croppper/flutter_customizable_image_croppper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_customizable_image_cropper/crop_widget/crop_widget.dart';
 
 void main() => runApp(const MyApp());
 
