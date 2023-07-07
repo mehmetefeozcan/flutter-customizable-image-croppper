3a8a75f06e5a943ef87dbc56ae0a77fcef0a41cf
mehmetefeozcan <ozcanmehmetefe@gmail.com>
2023-07-07 12:10:48 +0300

    update packages

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mehmetefeozcan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mehmetefeozcan/Desktop/workspace/flutter-customizable-image-croppper/flutter_customizable_image_croppper"
+export "FLUTTER_APPLICATION_PATH=/Users/mehmetefeozcan/Desktop/workspace/flutter-customizable-image-croppper"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.7"
+export "FLUTTER_BUILD_NUMBER=0.0.7"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
